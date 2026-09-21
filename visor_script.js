c11ccaa2b770c80a2fd08d05de7710902de9c5cf
leszlygallego01-coder <318311026+leszlygallego01-coder@users.noreply.github.com>
2026-09-21 11:22:40 -0500

    Update VISOR_API_URL to new endpoint

--- visor_script.js
+++ visor_script.js
@@ -12,7 +12,7 @@
 
 const LS_KEY_VISOR = 'MF_CONFIG_VISOR';
 
-const VISOR_API_URL = 'https://script.google.com/macros/s/AKfycbwpcGr2JkhEk_pPF5o4x8yd9o9kt0hC8eJDEWbjofoUl_zV2GSk_AExDHuCP2Xw1sFW/exec';
+const VISOR_API_URL = 'https://script.google.com/macros/s/AKfycby1w25gLyf6q-6WYOPfTbWD4BCSENLHSB2EnMjG-sdKRSvv5n3xbYEQ9gfg1IZoNM3C/exec';
 
 const VISOR_DEFAULTS = {
   apiUrl: VISOR_API_URL,
